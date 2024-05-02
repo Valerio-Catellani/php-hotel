9d4fbd5a97a9169e47f0554a8b1c32bc96e371da
Valerio-Catellani <valeriocatellani@gmail.com>
2024-05-02 17:28:44 +0200

    added some hotels

--- Models/hotel.php
+++ Models/hotel.php
@@ -1,6 +1,5 @@
 <?php
 $hotels = [
-
     [
         'name' => 'Hotel Belvedere',
         'description' => 'Hotel Belvedere Descrizione',
@@ -36,5 +35,74 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
-
+    [
+        'name' => 'Hotel Splendido',
+        'description' => 'Hotel Splendido Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 8.2
+    ],
+    [
+        'name' => 'Hotel Vista Mare',
+        'description' => 'Hotel Vista Mare Descrizione',
+        'parking' => false,
+        'vote' => 3,
+        'distance_to_center' => 3.7
+    ],
+    [
+        'name' => 'Hotel Panorama',
+        'description' => 'Hotel Panorama Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 2.8
+    ],
+    [
+        'name' => 'Hotel Sole',
+        'description' => 'Hotel Sole Descrizione',
+        'parking' => false,
+        'vote' => 4,
+        'distance_to_center' => 6.5
+    ],
+    [
+        'name' => 'Hotel Luna',
+        'description' => 'Hotel Luna Descrizione',
+        'parking' => true,
+        'vote' => 3,
+        'distance_to_center' => 12.1
+    ],
+    [
+        'name' => 'Hotel Mare e Monti',
+        'description' => 'Hotel Mare e Monti Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 9.8
+    ],
+    [
+        'name' => 'Hotel Le Palme',
+        'description' => 'Hotel Le Palme Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 6.9
+    ],
+    [
+        'name' => 'Hotel La Pineta',
+        'description' => 'Hotel La Pineta Descrizione',
+        'parking' => false,
+        'vote' => 2,
+        'distance_to_center' => 3.2
+    ],
+    [
+        'name' => 'Hotel I Tigli',
+        'description' => 'Hotel I Tigli Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 4.5
+    ],
+    [
+        'name' => 'Hotel Rosa',
+        'description' => 'Hotel Rosa Descrizione',
+        'parking' => false,
+        'vote' => 3,
+        'distance_to_center' => 7.7
+    ]
 ];
